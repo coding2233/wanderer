c96eddcad23442ba56c708e1011ae989865aa211
wanderer <dutifulwanderer@gmail.com>
2021-01-17 14:14:24 +0000

    暂时暂时reflector

--- src/base/reflector.cpp
+++ src/base/reflector.cpp
@@ -1,26 +1,26 @@
-#include "base/reflector.h"
-namespace wanderer
-{
-Reflector::Reflector(/* args */)
-{
-}
+// #include "base/reflector.h"
+// namespace wanderer
+// {
+// Reflector::Reflector(/* args */)
+// {
+// }
 
-Reflector::~Reflector()
-{
-}
+// Reflector::~Reflector()
+// {
+// }
 
-void Reflector::Register(const std::string &class_name, FUNC create_function)
-{
-    class_function_map_.insert(std::pair(class_name, create_function));
-}
+// void Reflector::Register(const std::string &class_name, FUNC create_function)
+// {
+//     class_function_map_.insert(std::pair(class_name, create_function));
+// }
 
-void Reflector::Create(const std::string &class_name)
-{
-    auto iter = class_function_map_.find(class_name);
-    if (iter != class_function_map_.end())
-    {
-        iter->second();
-        /* code */
-    }
-}
-}
+// void Reflector::Create(const std::string &class_name)
+// {
+//     auto iter = class_function_map_.find(class_name);
+//     if (iter != class_function_map_.end())
+//     {
+//         iter->second();
+//         /* code */
+//     }
+// }
+// }
--- src/base/reflector.h
+++ src/base/reflector.h
@@ -1,35 +1,35 @@
-#ifndef __REFLECTOR_H__
-#define __REFLECTOR_H__
+// #ifndef __REFLECTOR_H__
+// #define __REFLECTOR_H__
 
-#include <map>
-#include <memory>
-#include <functional>
+// #include <map>
+// #include <memory>
+// #include <functional>
 
-namespace wanderer
-{
-#define FUNC std::function<void *()>
+// namespace wanderer
+// {
+// #define FUNC std::function<void *()>
 
-class Reflector
-{
-private:
-    //函数类
-    std::map<std::string, FUNC> class_function_map_;
+// class Reflector
+// {
+// private:
+//     //函数类
+//     std::map<std::string, FUNC> class_function_map_;
 
-public:
-    Reflector(/* args */);
-    ~Reflector();
+// public:
+//     Reflector(/* args */);
+//     ~Reflector();
 
-    static Reflector *GetInstance()
-    {
-        static Reflector instance;
-        return &instance;
-    }
+//     static Reflector *GetInstance()
+//     {
+//         static Reflector instance;
+//         return &instance;
+//     }
 
-    //注册类名
-    void Register(const std::string &class_name, FUNC create_function);
-    //创建对象
-    void Create(const std::string &class_name);
-};
-} // namespace wanderer
+//     //注册类名
+//     void Register(const std::string &class_name, FUNC create_function);
+//     //创建对象
+//     void Create(const std::string &class_name);
+// };
+// } // namespace wanderer
 
-#endif
+// #endif
