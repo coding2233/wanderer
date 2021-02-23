e667052c0a5f2477c02881922d227989dae3c5e3
wanderer <gitea@fake.local>
2021-02-23 18:49:32 +0800

    实现IOCP

--- src/network/socket_iocp.cpp
+++ src/network/socket_iocp.cpp
@@ -27,17 +27,17 @@ namespace wanderer
     void SocketIOCP::Setup(std::function<void(int)> connected_callback, std::function<void(int, const char *data, int size)> receive_callback, std::function<void(const char name, int fd)> inner_connected_callback)
     {
         SocketBase::Setup(connected_callback, receive_callback, inner_connected_callback);
-        ////create iocp
-        //completion_port_ = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
-        ////create worker thread. Twice the number of cores.
-        //SYSTEM_INFO si;
-        //GetSystemInfo(&si);
-        //int worker_thread_num = si.dwNumberOfProcessors * 2;
-        //HANDLE* worker_threads = new HANDLE[worker_thread_num];
-        //for (int i = 0; i < worker_thread_num; i++)
-        //{
-        //    //worker_threads[i] = CreateThread(0, 0, WorkerThreadProc, (void*)this, 0, 0);
-        //}
+        //create iocp
+        completion_port_ = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+        //create worker thread. Twice the number of cores.
+        SYSTEM_INFO si;
+        GetSystemInfo(&si);
+        int worker_thread_num = si.dwNumberOfProcessors * 2;
+        worker_threads_ = new HANDLE[worker_thread_num];
+        for (int i = 0; i < worker_thread_num; i++)
+        {
+            worker_threads_[i] = CreateThread(0, 0, WorkerThreadProc, (void*)this, 0, 0);
+        }
         //logo
         SetLogo();
     }
@@ -48,7 +48,11 @@ namespace wanderer
 
     void SocketIOCP::Close()
     {
+        RELEASE_HANDLE(completion_port_);
+        RELEASE(worker_threads_);
+        RELEASE_SOCKET(listen_socket_);
     }
+
     int SocketIOCP::SendData(int fd, const char *data, size_t size)
     {
         return 0;
@@ -57,12 +61,52 @@ namespace wanderer
     //创建服务端监听socket
     int SocketIOCP::CreateListenSocket(const char *server_ip, int server_port)
     {
-        return 0;
+        listen_socket_=WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+        if (INVALID_SOCKET== listen_socket_)
+        {
+            throw std::runtime_error("Listening for socket invalid.");
+        }
+
+        // 将socket绑定到完成端口中
+        if (NULL == CreateIoCompletionPort((HANDLE)listen_socket_, completion_port_, (DWORD)listenSockContext, 0))
+        {
+            RELEASE_SOCKET(listen_socket_);
+            return false;
+        }
+
+        //服务器地址信息，用于绑定socket
+        sockaddr_in serverAddr;
+
+        // 填充地址信息
+        ZeroMemory((char*)&serverAddr, sizeof(serverAddr));
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = inet_addr(server_ip);
+        serverAddr.sin_port = htons(server_port);
+
+        // 绑定地址和端口
+        if (SOCKET_ERROR == bind(listen_socket_, (sockaddr*)&serverAddr, sizeof(serverAddr)))
+        {
+            throw std::runtime_error("Failed to listen for socket binding. ");
+        }
+
+        // 监听
+        if (SOCKET_ERROR == listen(listen_socket_, SOMAXCONN))
+        {
+            throw std::runtime_error("LisenSocket listen error.");
+        }
+
+        return listen_socket_;
     }
 
     //创建客户端的socket
     void SocketIOCP::CreateConnectSocket(const char name, const char *server_ip, int server_port)
     {
     }
+
+    DWORD SocketIOCP::WorkerThreadProc(LPVOID lpParam)
+    {
+        return 0;
+    }
+
 } // namespace wanderer
 #endif
