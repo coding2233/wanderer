af09c8f4ea840c689065b2e1f7bd4797fd8c88cc
wanderer <dutifulwanderer@gmail.com>
2021-03-23 23:19:14 +0000

    socket epoll

--- src/module/custom_module.cpp
+++ src/module/custom_module.cpp
@@ -3,11 +3,20 @@
 //This file is generated automatically by the program and is not allowed to be modified manually!
 
 #include "custom_module.h"
+#if WEBSERVER
+#include "WebServer/WebServer.h"
+#endif
 
 namespace wanderer
 {
 CustomModule::CustomModule(std::map<std::string, Module *> *modules, System *system)
 {
+
+#if WEBSERVER
+	WebServer *webserver__ = new WebServer(system);
+	modules->insert(std::pair<std::string, Module *>(typeid(*webserver__).name(),webserver__));
+#endif
+
 }
 
 CustomModule::~CustomModule()
--- src/network/socket_epoll.h
+++ src/network/socket_epoll.h
@@ -19,55 +19,54 @@ namespace wanderer
 
 #define MAX_EVENTS 1024
 
-class SocketEpoll : public SocketBase
-{
-private:
-    //服务端监听的socket
-    int listen_socket_;
-    //epoll的事件
-    struct epoll_event ev_, *events_;
-    //sock 地址
-    struct sockaddr_in server_addr_, client_addr_;
-    //客户端的长度
-    socklen_t client_len_;
-    //epoll instance
-    int epoll_fd_;
-    //有fd待处理数据的数目
-    int nfds_;
-    //连接的sock
-    int conn_sock_;
-    //等待时间
-    int sleep_time_ = 0;
+    class SocketEpoll : public SocketBase
+    {
+    private:
+        //服务端监听的socket
+        int listen_socket_;
+        //epoll的事件
+        struct epoll_event ev_, *events_;
+        //sock 地址
+        struct sockaddr_in server_addr_, client_addr_;
+        //客户端的长度
+        socklen_t client_len_;
+        //epoll instance
+        int epoll_fd_;
+        //有fd待处理数据的数目
+        int nfds_;
+        //连接的sock
+        int conn_sock_;
+        //等待时间
+        int sleep_time_ = 0;
 
-private:
-    //设置非阻塞模式
-    int SetNonblocking(int sfd);
-    //创建客户端段的socket
-    void CreateClientSocket(const char *name, const char *server_ip, int server_port, int sleep_time);
+    private:
+        //设置非阻塞模式
+        int SetNonblocking(int sfd);
+        //创建客户端段的socket
+        void CreateClientSocket(const char name, const char *server_ip, int server_port, int sleep_time);
 
-public:
-    SocketEpoll(/* args */);
-    ~SocketEpoll();
-    //设置
-    virtual void Setup(std::function<void(int)> connected_callback, std::function<void(int, const char *data, int size)> receive_callback, std::function<void(const char name, int fd)> inner_connected_callback) override;
-    //循环
-    void Loop() override;
-    //关闭
-    void Close() override;
-    //发送数据
-    int SendData(int fd, const char *data, size_t size) override;
-    //创建服务端监听socket
-    int CreateListenSocket(const char *server_ip, int server_port) override;
-    //创建客户端的socket
-    void CreateConnectSocket(const char name, const char *server_ip, int server_port) override;
+    public:
+        SocketEpoll(/* args */);
+        ~SocketEpoll();
+        //设置
+        virtual void Setup(std::function<void(int)> connected_callback, std::function<void(int, const char *data, int size)> receive_callback, std::function<void(const char name, int fd)> inner_connected_callback) override;
+        //循环
+        void Loop() override;
+        //关闭
+        void Close() override;
+        //发送数据
+        int SendData(int fd, const char *data, size_t size) override;
+        //创建服务端监听socket
+        int CreateListenSocket(const char *server_ip, int server_port) override;
+        //创建客户端的socket
+        void CreateConnectSocket(const char name, const char *server_ip, int server_port) override;
 
-protected:
-    //设置logo
-    void SetLogo() override;
-};
+    protected:
+        //设置logo
+        void SetLogo() override;
+    };
 
 } // namespace wanderer
 #endif
-
 
 #endif
