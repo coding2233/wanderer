6daedeceec6da1faaf0493412261ae6197fd6b63
wanderer <dutifulwanderer@gmail.com>
2021-03-24 09:56:41 +0000

    github ci

--- auto.sh
+++ auto.sh
@@ -2,6 +2,7 @@
 
 cd ./docker/docker-build
 docker-compose up --build
+# docker rm -f wanderer-x
 
 # cd ../run
 # cd ../../build
@@ -9,7 +10,6 @@ docker-compose up --build
 cd ../../
 
 cp -r ./docker/run ./build/
-
 cp ./build/wanderer ./build/run/
 
 # ./wanderer -t all --server 0.0.0.0:2233 --gate_server 0.0.0.0:2233
