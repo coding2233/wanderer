0df26dc3575265928b1bf108db7227c2c4df0d17
wanderer <dutifulwanderer@gmail.com>
2021-05-06 18:12:34 +0800

    测试登陆成功

--- src/login/login_module.cpp
+++ src/login/login_module.cpp
@@ -1,5 +1,6 @@
 #include "login/login_module.h"
 #include "network/message.h"
+#include "utility/jsonrpcpp.hpp"
 #include <string>
 
 namespace wanderer
@@ -54,10 +55,19 @@ namespace wanderer
         {
             if (entity->is_request()) 
             {
-                auto request = std::dynamic_pointer_cast<jsonrpcpp::Request>(entity);
+                jsonrpcpp::request_ptr request = std::dynamic_pointer_cast<jsonrpcpp::Request>(entity);
                 if(request->method()=="login")
                 {
+                    // jsonrpcpp::Response(*request, result);
+                    auto user_name = request->params().get<std::string>(0);
                     LOG(INFO)<<"LOGIN MODULE: "<<request->to_json().dump();
+                    auto respone= jsonrpcpp::Response(*request,Json({true,"登陆成功！！！dsfsd#"}));
+                   
+                    auto data= respone.to_json().dump();
+                    std::string message_data(data.c_str(),data.size());
+                    Message message;
+                    message.Setup(MessageType_2L,message_data.c_str(),message_data.size());
+                    session->Send(&message);
                     // auto user_name = request->params().get<std::string>("user_name")
                 }
             }
